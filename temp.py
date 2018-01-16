54fd970736f0ec909acdf4376b5dff47ddfefd66
Abby Noble <tech.substance@gmail.com>
2018-01-16 01:45:42 -0800

    Update temp.py
    Changed "eval" to "float". Eval vulnerable to exploitation via injected code.

--- temp.py
+++ temp.py
@@ -1,7 +1,7 @@
 # Temp converter.
 
 def translate():
-     c = eval(input("Type temp here: "))
+     c = float(input("Type temp here: "))
      f = 9 / 5 * c + 32
      print("You're answer is here ", f)
 
