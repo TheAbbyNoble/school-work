8db0f630d5015ffe704bcf7831286d731337af36
Abby Noble <tech.substance@gmail.com>
2018-01-17 20:28:54 -0800

    Create APR_Calc.py
    Made in class

---
+++ APR_Calc.py
@@ -0,0 +1,13 @@
+# A quick calc program to track expenses
+def main():
+  print("Calculate ten year investiment")
+  principle = float(input("Enter principle: "))
+
+  apr = float(input("Annual APR: "))
+
+  for i in range(10):
+      principle = principle * (1 + apr)
+
+  print("Investment value after 10 years is:", principle)
+
+main() 
